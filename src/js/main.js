db80e56516ef8c6ac1f5ccba96292810a1dda7a3
TetianaChehrina <3909139@gmail.com>
2024-03-24 14:10:29 +0200

    HW pixabay

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import './css/loader-styles.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
---
+++ src/js/pixabay-api.js
@@ -0,0 +1,2 @@
+const API_KEY = '43043595-2b9ab8fff7b2b720f98fadc4f';
+const baseURL = 'https://pixabay.com/api/';
